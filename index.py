65677293a2d9b9a493f1063ffd6bcb0c69697282
gabrieltrouve <gabrieltrouve5@gmail.com>
2024-12-17 21:34:34 +0100

    update index

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ import requests
 
 from func import frenchy_format
 
+# https://developer.themoviedb.org/reference/movie-now-playing-list
+
 
 url = "https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&region=FR"
 
@@ -12,13 +14,15 @@ headers = {
 }
 
 response = requests.get(url, headers=headers).json()
+print(type(response))
+print(response)
 
 
 st.title("Les films du moment")
 
 for el in response["results"]:
     st.header(el["title"])
-    st.image(f"https://image.tmdb.org/t/p/w500/{el['poster_path']}")
+    st.image(f"https://image.tmdb.org/t/p/w500{el['poster_path']}")
     st.subheader(el["overview"])
     st.caption(f"Sortie le {frenchy_format(el['release_date'])}")
     st.divider()
